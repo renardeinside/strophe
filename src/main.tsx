89aa4e6bb0decc723b931eae34c1c9d677a85bea
renardeinside <polarpersonal@gmail.com>
2024-11-17 13:21:47 +0100

    add commentary

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './tailwind.css'
 
-// @ts-expect-error - for some reason this doesn't work
+// @ts-expect-error - for some reason this doesn't work in terms of importing the font
+// however, it works in the browser
 import '@fontsource-variable/fira-code';
 
 createRoot(document.getElementById('root')!).render(
