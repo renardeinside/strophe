8c75d075ec28f9da5f274c4d98f7b369ecb2cd4b
renardeinside <polarpersonal@gmail.com>
2024-11-23 21:55:20 +0100

    fix tests

--- tests/font-size.spec.ts
+++ tests/font-size.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from "./fixtures";
 test("font size should be bigger", async ({ newtab }) => {
     const input = await newtab.waitForSelector(".ProseMirror");
     const fontSize = await input.evaluate((node: any) => window.getComputedStyle(node).fontSize);
-    expect(fontSize).toBe("24px");
+    expect(fontSize).toBe("16px");
 });
