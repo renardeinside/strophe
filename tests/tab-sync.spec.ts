de3429963d8772d6bbfe02744f77c3ad3637b823
renardeinside <polarpersonal@gmail.com>
2024-11-23 22:19:15 +0100

    add tabs sync test

--- tests/tab-sync.spec.ts
+++ tests/tab-sync.spec.ts
@@ -1,9 +1,19 @@
-import { test, expect,getPreparedTab  } from "./fixtures";
+import { test, expect, getPreparedTab } from "./fixtures";
+import { loremIpsum } from "lorem-ipsum";
 
 test("input should be synced across tabs", async ({ context }) => {
-    const tab1 = await getPreparedTab(context);
-    const tab2 = await getPreparedTab(context);
-    const input1 = await tab1.waitForSelector(".ProseMirror");
-    const input2 = await tab2.waitForSelector(".ProseMirror");
+  const tab1 = await getPreparedTab(context);
+  const tab2 = await getPreparedTab(context);
 
-}
+  const input1 = await tab1.getByRole("textbox");
+  const input2 = await tab2.getByRole("textbox");
+
+  const testText = loremIpsum({ count: 1, units: "sentences" });
+
+  await input1.pressSequentially(testText, { delay: 10 }); // type some text in the first tab, slowly like a human
+
+  // wait for 20ms to let the text sync
+  await new Promise((resolve) => setTimeout(resolve, 20));
+
+  await expect(input2).toHaveText(testText); // wait for the second tab to sync
+});
